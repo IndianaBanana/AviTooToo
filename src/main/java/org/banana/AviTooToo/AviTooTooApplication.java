79b46b6665efe5e52364afef196fb81b36ed2dd2
IndianaBanana <kuzmasec@gmail.com>
2025-04-13 13:16:06 +0300

    Initial commit

---
+++ src/main/java/org/banana/AviTooToo/AviTooTooApplication.java
@@ -0,0 +1,13 @@
+package org.banana.AviTooToo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviTooTooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviTooTooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/banana/AviTooToo/AviTooTooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.banana.AviTooToo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviTooTooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
