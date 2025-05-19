d33fdda83f8c56bd83ee55117b44c5609da70059
IndianaBanana <alkuzhelev@gmail.com>
2025-05-19 23:34:25 +0300

    bycrypt strength to 12

--- src/main/java/org/banana/config/SecurityConfig.java
+++ src/main/java/org/banana/config/SecurityConfig.java
@@ -64,6 +64,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(16);
+        return new BCryptPasswordEncoder(12);
     }
 }
